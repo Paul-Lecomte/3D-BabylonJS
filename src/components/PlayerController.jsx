62c720af064e6af54a3b2d82fbe517a4188867b0
Paul <lecomtepaulvd@gmail.com>
2025-04-28 10:38:46 +0200

    player

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -1,6 +1,22 @@
 // PlayerController.jsx
 import React, { useEffect, useRef, useContext } from 'react';
-import { MeshBuilder, TransformNode, Vector3, Scalar } from '@babylonjs/core';
+import {MeshBuilder, TransformNode, Vector3, Scalar, StandardMaterial, Color3} from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 import { GRAVITY, PLAYER_SPEED, PLAYER_JUMP_FORCE, ROTATION_SPEED } from '../settings/const';
 
+export const PlayerController = () => {
+    const {scene} = useContext(GameObjectContext);
+
+    useEffect(() => {
+
+        const player = MeshBuilder.CreateSphere("player", {diameter: 1}, scene);
+        player.position = new Vector3(0, 0, 0);
+
+        //player material
+        const playerMatetial = new StandardMaterial("playerMaterial", scene)
+        playerMatetial.diffuseColor = new Color3(0, 0, 1);
+    }, [scene]);
+
+
+    return null;
+}
--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { GameObject } from '../contexts/GameObjectContext';
 //import {CameraController} from './CameraController';
 import {EnvironmentController} from './EnvironnementController';
-//import {PlayerController} from './PlayerController';
+import {PlayerController} from './PlayerController';
 //import {InputController} from './InputController';
 
 export const Shell = ({ scene, engine }) => {
@@ -10,8 +10,9 @@ export const Shell = ({ scene, engine }) => {
   return (
     <GameObject scene={scene} engine={engine}>
       <EnvironmentController />
+
+       <PlayerController /*input={inputValues} onPlayerCreated={handlePlayerCreated}*/ />
         {/*
-       <PlayerController input={inputValues} onPlayerCreated={handlePlayerCreated} />
       {playerMesh && <CameraController playerMesh={playerMesh} />}
       <InputController onUpdate={handleInputUpdated} />
       */}
