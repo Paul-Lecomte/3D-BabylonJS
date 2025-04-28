ef7cdfbfb77ccf21873422d73ff70e7163422dc1
Paul <lecomtepaulvd@gmail.com>
2025-04-28 10:54:32 +0200

    set square to see rotation of player

--- src/components/PlayerController.jsx
+++ src/components/PlayerController.jsx
@@ -1,6 +1,14 @@
 // PlayerController.jsx
 import React, { useEffect, useRef, useContext } from 'react';
-import {MeshBuilder, TransformNode, Vector3, Scalar, StandardMaterial, Color3} from '@babylonjs/core';
+import {
+    MeshBuilder,
+    TransformNode,
+    Vector3,
+    Scalar,
+    StandardMaterial,
+    Color3,
+    Mesh as meshBuilder
+} from '@babylonjs/core';
 import { GameObjectContext } from '../contexts/GameObjectContext';
 import { GRAVITY, PLAYER_SPEED, PLAYER_JUMP_FORCE, ROTATION_SPEED } from '../settings/const';
 
@@ -9,12 +17,21 @@ export const PlayerController = () => {
 
     useEffect(() => {
 
-        const player = MeshBuilder.CreateSphere("player", {diameter: 1}, scene);
-        player.position = new Vector3(0, 0, 0);
+        const player = MeshBuilder.CreateSphere("player", {diameter: 2}, scene);
+        player.position = new Vector3(0, 1, 0);
 
         //player material
         const playerMatetial = new StandardMaterial("playerMaterial", scene)
         playerMatetial.diffuseColor = new Color3(0, 0, 1);
+
+        player.checkCollisions = true;
+
+        const node = new TransformNode("playerNode", scene);
+        node.parent = player;
+        node.position.set(0, 0, 1);
+        const square = MeshBuilder.CreateBox("square", {size: 0.5}, scene);
+        square.parent = node
+
     }, [scene]);
 
 
